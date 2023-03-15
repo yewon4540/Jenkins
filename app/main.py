7e7c57feddf02722296d7dcc7104d1109d85f81e
root <root@BOOK-4H3SDLR4UV>
2023-03-15 16:38:56 +0900

    test main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,11 @@
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-import mlflow
-import mlflow.sklearn
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-X = np.array([-1, -2, 2, 1, 2, 1]).reshape(-1, 1)
-y = np.array([1, 1, 0, 0, 1, 0])
-lr = LogisticRegression()
-lr.fit(X, y)
-score = lr.score(X, y)
-# print("Score: %s" % score)
-mlflow.log_metric("score", score)
-mlflow.sklearn.log_model(lr, "model")
-# print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
 
 @app.get("/")
 def read_root():
-    return {"Score: %s": f"{score}", "Model saved in run %s":f"{mlflow.active_run().info.run_uuid}"}
+    return {"Hello": "MLOps", "Bye":"MLOps"}
 
 
 @app.get("/items/{item_id}")
