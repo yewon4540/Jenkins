5439b19a913a7162b425fb1eb7a43f7db5936e76
root <root@BOOK-4H3SDLR4UV>
2023-03-15 16:34:05 +0900

    test main.py

--- app/main.py
+++ app/main.py
@@ -17,13 +17,17 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-if __name__ == "__main__":
+@app.get("/items/{item_id}")
+def logistic():
     X = np.array([-1, -2, 2, 1, 2, 1]).reshape(-1, 1)
     y = np.array([1, 1, 0, 0, 1, 0])
     lr = LogisticRegression()
     lr.fit(X, y)
     score = lr.score(X, y)
-    print("Score: %s" % score)
+    # print("Score: %s" % score)
     mlflow.log_metric("score", score)
     mlflow.sklearn.log_model(lr, "model")
-    print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
+    # print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
+
+    return "Score: %s" % score
+    return "Model saved in run %s" % mlflow.active_run().info.run_uuid
